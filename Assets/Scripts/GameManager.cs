5a8768c4b6b097cb583696a12e24361203af3d26
Nixon <voxell.technologies@gmail.com>
2023-04-15 12:45:33 +0800

    feat: add set vignette

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     [SerializeField] private Camera m_MainCamera;
+    [SerializeField] private Volume m_GlobalVolume;
+
+    private Vignette m_Vignette;
 
     public Camera MainCamera => this.m_MainCamera;
+    public Volume GlobalVolume => this.m_GlobalVolume;
+    public Vignette Vignette => this.m_Vignette;
 
     private void Awake()
     {
+        this.m_GlobalVolume.profile.TryGet<Vignette>(out this.m_Vignette);
         if (Instance == null)
         {
             Instance = this;
@@ -18,6 +26,11 @@ public class GameManager : MonoBehaviour
             Debug.LogWarning("There is probably more than one instance.", Instance);
             Object .Destroy(this);
         }
+    }
+
+    public void SetVignetteIntensity(float intensity)
+    {
+        this.Vignette.intensity.value = intensity;
     }
 
     private void OnDestroy()
