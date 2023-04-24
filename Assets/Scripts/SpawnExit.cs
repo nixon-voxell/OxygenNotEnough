1f6206d6a19082f6e0d939939555093c80bbd6ac
Nixon <voxell.technologies@gmail.com>
2023-04-24 23:57:30 +0800

    asdas

--- Assets/Scripts/SpawnExit.cs
+++ Assets/Scripts/SpawnExit.cs
@@ -6,7 +6,7 @@ public class SpawnExit : MonoBehaviour
     {
         MazeGenerator mazeGenerator = GameManager.Instance.MazeGenerator;
 
-        mazeGenerator.PlaceObject(this.transform, mazeGenerator.Width - 1, mazeGenerator.Height - 1);
+        mazeGenerator.PlaceObject(this.transform, mazeGenerator.Width, mazeGenerator.Height);
         // mazeGenerator.SetSize(this.transform, 0.25f, 1.0f, 1.5f);
         this.transform.localScale = new Vector3(0.25f, 1.0f, 1.5f);
     }
